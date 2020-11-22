0f0ed616b1f12804840c6be2cc964a2495a536d4
harimandir <harimandir@outlook.com>
2020-11-21 22:07:24 -0700

    Add a max chars solution using an object as a map

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,9 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {
+const maxChar = maxCharWithObject;
+
+function maxCharWithObject(str) {
+  const map = {};
+  for (const char of str.split("")) {
+    map[char] = map[char]++ || 1;
+  }
+
+  let maxVal = 0,
+    maxChar = "";
+  for (const char in map) {
+    if (map[char] > maxVal) {
+      maxVal = map[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
+
+function maxCharWithMap(str) {
   const map = new Map();
-  for (char of str.split("")) {
+  for (const char of str.split("")) {
     if (map.has(char)) {
       map.set(char, map.get(char) + 1);
     } else {
@@ -17,7 +36,7 @@ function maxChar(str) {
 
   let maxVal = 0,
     maxChar = "";
-  for ([char, val] of map) {
+  for (const [char, val] of map) {
     if (val > maxVal) {
       maxVal = val;
       maxChar = char;
