d005df2cd8996ce43d37c93c147ad95dfe3c735c
harimandir <harimandir@outlook.com>
2020-11-21 21:50:55 -0700

    Implement a solution for maxChar using a Map

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const map = new Map();
+  for (char of str.split("")) {
+    if (map.has(char)) {
+      map.set(char, map.get(char) + 1);
+    } else {
+      map.set(char, 1);
+    }
+  }
+
+  let maxVal = 0,
+    maxChar = "";
+  for ([char, val] of map) {
+    if (val > maxVal) {
+      maxVal = val;
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
