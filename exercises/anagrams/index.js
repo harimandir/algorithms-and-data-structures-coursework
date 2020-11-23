b9b2d5fbdb4a1c330b8064c2346b7c8aa24c40c6
harimandir <harimandir@outlook.com>
2020-11-22 21:35:31 -0700

    Implement a solution for anagram string comparison

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,14 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  return (
+    strToLowerCaseSortedChars(stringA) === strToLowerCaseSortedChars(stringB)
+  );
+}
+
+function strToLowerCaseSortedChars(str) {
+  return str.replace(/\W/g).toLowerCase().split("").sort().join("");
+}
 
 module.exports = anagrams;
