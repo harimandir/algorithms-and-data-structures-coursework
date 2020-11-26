b0eb2a1a647629544c5ea8b1affb1420fa2b471f
harimandir <harimandir@outlook.com>
2020-11-25 23:00:29 -0700

    Refactor recursive solution to improve readability

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -16,28 +16,26 @@
 
 const pyramid = pyramidWithRecursion;
 
-function pyramidWithRecursion(
-  n,
-  baseWidth = 2 * n - 1,
-  midpoint = Math.floor((baseWidth + 1) / 2),
-  row = n,
-  col = 1,
-  level = ""
-) {
-  if (row < 1) {
+function pyramidWithRecursion(n, row = 1, level = "") {
+  if (row > n) {
     return;
   }
-  if (col <= baseWidth) {
-    if (col > midpoint - row && col < midpoint + row) {
-      level += "#";
-    } else {
-      level += " ";
-    }
-    pyramid(n, baseWidth, midpoint, row, col + 1, level);
+  const baseWidth = 2 * n - 1;
+  const midpoint = Math.floor((baseWidth + 1) / 2);
+  const col = level.length + 1;
+
+  if (col > baseWidth) {
+    console.log(level);
+    pyramid(n, row + 1, "");
     return;
   }
-  pyramid(n, baseWidth, midpoint, row - 1, 1, "");
-  console.log(level);
+
+  if (col > midpoint - row && col < midpoint + row) {
+    level += "#";
+  } else {
+    level += " ";
+  }
+  pyramid(n, row, level);
 }
 
 function pyramidWithRecursionAndLoop(
