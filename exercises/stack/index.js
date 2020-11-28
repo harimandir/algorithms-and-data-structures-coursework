e2e25462beb71bab681bf75d44d7cae649a39a74
harimandir <harimandir@outlook.com>
2020-11-27 20:54:51 -0700

    Add a stack solution

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,20 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+  elements = [];
+
+  push(element) {
+    this.elements.push(element);
+  }
+
+  pop() {
+    return this.elements.pop();
+  }
+
+  peek() {
+    return this.elements[this.elements.length - 1];
+  }
+}
 
 module.exports = Stack;
