68dd885e1096f1ae3a6e4cea954c93c985f73e5a
harimandir <harimandir@outlook.com>
2020-11-27 21:54:19 -0700

    Implement a queue that switches stacks when reading

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -19,6 +19,35 @@ class Queue {
   stash = new Stack();
 
   add(element) {
+    this.stack.push(element);
+  }
+
+  remove() {
+    this._switchStacks(this.stack, this.stash);
+    let element = this.stash.pop();
+    this._switchStacks(this.stash, this.stack);
+    return element;
+  }
+
+  _switchStacks(from, to) {
+    while (from.peek() !== undefined) {
+      to.push(from.pop());
+    }
+  }
+
+  peek() {
+    this._switchStacks(this.stack, this.stash);
+    let element = this.stash.peek();
+    this._switchStacks(this.stash, this.stack);
+    return element;
+  }
+}
+
+class QueueFromStacksOnAdd {
+  stack = new Stack();
+  stash = new Stack();
+
+  add(element) {
     while (this.stack.peek() !== undefined) {
       this.stash.push(this.stack.pop());
     }
