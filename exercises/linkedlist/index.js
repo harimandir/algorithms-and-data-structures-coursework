a6471808301994491098ef2cfa70fec2438f0182
harimandir <harimandir@outlook.com>
2020-11-28 17:21:19 -0700

    Make the linked list iterable

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -97,6 +97,14 @@ class LinkedList {
       node = node.next;
     }
   }
+
+  *[Symbol.iterator]() {
+    let node = this.head;
+    while (node) {
+      yield node;
+      node = node.next;
+    }
+  }
 }
 
 module.exports = { Node, LinkedList };
