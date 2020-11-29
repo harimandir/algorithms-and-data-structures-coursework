d12b6f9c481989f40da0976c36f258d6df0864d7
harimandir <harimandir@outlook.com>
2020-11-29 14:16:45 -0700

    Include a naive brute force solution for level width

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,31 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let node = root,
+    queue = [root],
+    i = 0;
+  while (node) {
+    queue.push(...node.children);
+    node = queue[++i];
+  }
+  let widths = [1],
+    level = 0;
+  node = queue.shift();
+  while (node) {
+    if (node.children.length) {
+      widths[level + 1] = node.children.length;
+    }
+    for (i = 1; i < widths[level]; i++) {
+      if (queue[0]?.children.length) {
+        widths[level + 1] += queue[0].children.length;
+      }
+      node = queue.shift();
+    }
+    level++;
+    node = queue.shift();
+  }
+  return widths;
+}
 
 module.exports = levelWidth;
