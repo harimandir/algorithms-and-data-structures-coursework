b7dd4f7a72b8d01f0cb36277a3b8a4a2a7b73096
harimandir <harimandir@outlook.com>
2020-11-29 14:45:56 -0700

    Solve level width using queue step markers

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -12,28 +12,25 @@
 // Answer: [1, 3, 2]
 
 function levelWidth(root) {
-  let node = root,
-    queue = [root],
-    i = 0;
+  let prev,
+    node = root,
+    step = "|",
+    queue = [root, step],
+    level = 0,
+    widths = [];
   while (node) {
-    queue.push(...node.children);
-    node = queue[++i];
-  }
-  let widths = [1],
-    level = 0;
-  node = queue.shift();
-  while (node) {
-    if (node.children.length) {
-      widths[level + 1] = node.children.length;
-    }
-    for (i = 1; i < widths[level]; i++) {
-      if (queue[0]?.children.length) {
-        widths[level + 1] += queue[0].children.length;
-      }
-      node = queue.shift();
-    }
-    level++;
     node = queue.shift();
+    if (node === prev) {
+      break;
+    }
+    prev = node;
+    if (node === step) {
+      queue.push(node);
+      level++;
+    } else {
+      queue.push(...node.children);
+      widths[level] = (widths[level] || 0) + 1;
+    }
   }
   return widths;
 }
