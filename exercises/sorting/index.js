3428cbc71aa104c03a38b71ad400d1fbbc405b58
harimandir <harimandir@outlook.com>
2020-11-30 10:29:35 -0700

    Add a selection sort implementation

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -15,7 +15,19 @@ function bubbleSort(arr) {
   return arr;
 }
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[minIndex] > arr[j]) {
+        let min = arr[j];
+        arr[j] = arr[minIndex];
+        arr[minIndex] = min;
+      }
+    }
+  }
+  return arr;
+}
 
 function mergeSort(arr) {}
 
