a96e6f8ea4fe32e6e77afc33fec59a5aa7cbb327
harimandir <harimandir@outlook.com>
2020-11-30 09:55:18 -0700

    Implement a bubble sort algorithm

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,19 +2,23 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  for (let i = arr.length - 1; i > 1; i--) {
+    for (let j = 0; j < i; j++) {
+      const left = arr[j];
+      const right = arr[j + 1];
+      if (left > right) {
+        arr[j] = right;
+        arr[j + 1] = left;
+      }
+    }
+  }
+  return arr;
 }
 
-function selectionSort(arr) {
+function selectionSort(arr) {}
 
-}
+function mergeSort(arr) {}
 
-function mergeSort(arr) {
-
-}
-
-function merge(left, right) {
-
-}
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
