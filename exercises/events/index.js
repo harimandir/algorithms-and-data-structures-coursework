43605d57281bb6bcc45e55d7600eb87a005d2a24
harimandir <harimandir@outlook.com>
2020-11-30 09:18:42 -0700

    Implement a simple eventing library

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,34 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.register = {};
+  }
+
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if (this.register[eventName]) {
+      this.register[eventName].push(callback);
+    } else {
+      this.register[eventName] = [callback];
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if (this.register[eventName]) {
+      for (let handler of this.register[eventName]) {
+        handler();
+      }
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    delete this.register[eventName];
+  }
 }
 
 module.exports = Events;
